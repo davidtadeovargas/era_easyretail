916a1bb4dd1d85afc988d575f370ad242f8f644c
david <coritocorito@hotmail.com>
2020-09-22 23:02:16 -0600

    Varios ajustes
    -Crear el repositorio de base de datos del catálogo de bancos
    -Crear el combobox de bancos
    -Que el componente se pueda jalar a la paleta de netbeans
    -Que cuando se abre la ventana de catálogo de bancos se cargue la lista de bancos en el combobox
    -Cuando se selecciona un elemento del combobox del catálogo de bancos que coloque la descripción del banco en su campo correspondiente
    -Crear la tabla de catálogo de cuentas
    -Cuando la pantalla de catálogo de bancos se abre que cargue la tabla de catálogo de cuentas correctamente
    -Que se pueda dar de alta una cuenta del catálogo de cuentas
    -Que se pueda borrar una cuenta del catálogo de cuentas
    -En el catálogo de cuentas cuando se selecciona una cuenta de la tabla que cargue los datos en los campos correspondientes
    -Que se pueda editar una cuenta del catálogo de cuentas
    -En el catálogo de cuentas cuando se selecciona una cuenta para editar que no permita modificar la cuenta, la demas información si
    -En el catálogo de cuentas que el botón de busqueda por filtro funcione
    -En el catálogo de cuentas que el botón de mostrar todos los registros funcione
    -En el catálogo de cuentas que no permita agregar una cuenta si ya existe
    -En el repositorio de catálogo de cuentas agregar un método para obtener el registro por cuenta

--- src/main/java/com/era/easyretail/controllers/views/CatalBancosViewController.java
+++ src/main/java/com/era/easyretail/controllers/views/CatalBancosViewController.java
@@ -8,9 +8,14 @@ package com.era.easyretail.controllers.views;
 import com.era.views.CatalBancosJFrame;
 import java.util.List;
 import com.era.logger.LoggerUtility;
+import com.era.models.Banco;
+import com.era.models.Banks;
+import com.era.repositories.RepositoryFactory;
 import com.era.views.dialogs.DialogsFactory;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.event.ListSelectionEvent;
 
 /**
  *
@@ -23,12 +28,6 @@ public class CatalBancosViewController extends CatalBancosJFrame {
         
         try{
                      
-            jBNew1.addActionListener((java.awt.event.ActionEvent evt) -> {
-                jBNew1ActionPerformed(evt);
-            });
-            jBImportar.addActionListener((java.awt.event.ActionEvent evt) -> {
-                jBImportarActionPerformed(evt);
-            });
             jBActua.addActionListener((java.awt.event.ActionEvent evt) -> {
                 jBActuaActionPerformed(evt);
             });
@@ -47,6 +46,41 @@ public class CatalBancosViewController extends CatalBancosJFrame {
             jBDel.addActionListener((java.awt.event.ActionEvent evt) -> {
                 jBDelActionPerformed(evt);
             });
+            
+            banksCombobox.setChangeSelectionListener((Object ObjectModel) -> {
+                
+                final Banks Bank = (Banks)ObjectModel;
+                
+                jTDescripBank.setText(Bank.getDescrip());
+                jTDescripBank.setCaretPosition(0);
+            });
+            banksCombobox.loadItems();
+                 
+            bancoTable.setITableRowSelected((ListSelectionEvent lse, Object Object) -> {
+                
+                try {
+                 
+                    final Banco Banco = (Banco)Object;
+
+                    final Banks Bank = (Banks)RepositoryFactory.getInstance().getBanksRepository().getByCode(Banco.getBanco());
+                    
+                    jTCod.setText(Banco.getCuentabanco());
+                    jTDescrip.setText(Banco.getDescrip());
+                    banksCombobox.selectByObject(Bank);
+                    
+                    jTCod.setEditable(false);
+                    
+                } catch (Exception ex) {
+                    LoggerUtility.getSingleton().logError(this.getClass(), ex);
+                    try {
+                        DialogsFactory.getSingleton().getExceptionDialog(baseJFrame, ex).show();
+                    } catch (Exception ex1) {
+                        Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex1);
+                    }
+                }
+            });
+            bancoTable.showCommonColumns();
+            bancoTable.loadAllItemsInTable();
             
         }catch (Exception ex) {
             LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
@@ -69,7 +103,35 @@ public class CatalBancosViewController extends CatalBancosJFrame {
     private void jBDelActionPerformed(java.awt.event.ActionEvent evt) {                                             
 
 	try{            	
+    
+            final Banco Banco = (Banco)bancoTable.getRowSelected();
+            if(Banco==null){
+                DialogsFactory.getSingleton().showErrorOKNoSelectionCallbackDialog(baseJFrame, (JFrame jFrame) -> {
+                    bancoTable.grabFocus();
+                });
+                return;
+            }
             
+            DialogsFactory.getSingleton().showQuestionContinueDialog(baseJFrame, (JFrame jFrame) -> {
+                
+                try {
+                    
+                    RepositoryFactory.getInstance().getBancosRepository().delete(Banco);
+                    
+                    bancoTable.loadAllItemsInTable();
+                    
+                    DialogsFactory.getSingleton().showOKOperationCompletedCallbackDialog(jFrame, (JFrame jFrame1) -> {
+                    });
+                    
+                } catch (Exception ex) {
+                    LoggerUtility.getSingleton().logError(this.getClass(), ex);
+                    try {
+                        DialogsFactory.getSingleton().getExceptionDialog(baseJFrame, ex).show();
+                    } catch (Exception ex1) {
+                        Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex1);
+                    }
+                }
+            });
 	}
 	catch (Exception ex) {
             LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
@@ -84,7 +146,22 @@ public class CatalBancosViewController extends CatalBancosJFrame {
     private void jBSalActionPerformed(java.awt.event.ActionEvent evt) {                                             
 
 	try{            	
-            
+    
+            DialogsFactory.getSingleton().showQuestionContinueDialog(baseJFrame, (JFrame jFrame) -> {
+                
+                try {
+                    
+                    dispose();
+                    
+                } catch (Exception ex) {
+                    LoggerUtility.getSingleton().logError(this.getClass(), ex);
+                    try {
+                        DialogsFactory.getSingleton().getExceptionDialog(baseJFrame, ex).show();
+                    } catch (Exception ex1) {
+                        Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex1);
+                    }
+                }
+            });
 	}
 	catch (Exception ex) {
             LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
@@ -100,6 +177,63 @@ public class CatalBancosViewController extends CatalBancosJFrame {
 
 	try{            	
             
+            final String account = jTCod.getText().trim();
+            if(account.isEmpty()){
+                DialogsFactory.getSingleton().showOKEmptyFieldCallbackDialog(baseJFrame, (JFrame jFrame) -> {
+                    jTCod.grabFocus();
+                });
+                return;
+            }
+            
+            final Banco Banco_= RepositoryFactory.getInstance().getBancosRepository().getByAccount(account);
+            if(Banco_!=null){
+                DialogsFactory.getSingleton().showErrorRecordExistsOKDialog(baseJFrame, (JFrame jFrame1) -> {
+                    jTCod.grabFocus();
+                });
+                return;
+            }
+            
+            final String description = jTDescrip.getText().trim();
+            if(description.isEmpty()){
+                DialogsFactory.getSingleton().showOKEmptyFieldCallbackDialog(baseJFrame, (JFrame jFrame) -> {
+                    jTDescrip.grabFocus();
+                });
+                return;
+            }
+            
+            final Banks Bank = (Banks)banksCombobox.getSelectedObject();
+            if(Bank.getName().isEmpty()){
+                DialogsFactory.getSingleton().showOKEmptyFieldCallbackDialog(baseJFrame, (JFrame jFrame) -> {
+                    banksCombobox.grabFocus();
+                });
+                return;
+            }
+                    
+            DialogsFactory.getSingleton().showQuestionContinueDialog(baseJFrame, (JFrame jFrame) -> {
+                
+                try {
+                    
+                    final Banco Banco = new Banco();
+                    Banco.setBanco(Bank.getCode());
+                    Banco.setCuentabanco(account);
+                    Banco.setDescrip(description);
+                    
+                    RepositoryFactory.getInstance().getBancosRepository().save(Banco);
+                    
+                    bancoTable.loadAllItemsInTable();
+                    
+                    DialogsFactory.getSingleton().showOKOperationCompletedCallbackDialog(jFrame, (JFrame jFrame1) -> {
+                    });
+                    
+                } catch (Exception ex) {
+                    LoggerUtility.getSingleton().logError(this.getClass(), ex);
+                    try {
+                        DialogsFactory.getSingleton().getExceptionDialog(baseJFrame, ex).show();
+                    } catch (Exception ex1) {
+                        Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex1);
+                    }
+                }
+            });
 	}
 	catch (Exception ex) {
             LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
@@ -113,8 +247,18 @@ public class CatalBancosViewController extends CatalBancosJFrame {
     
     private void jBBuscActionPerformed(java.awt.event.ActionEvent evt) {                                             
 
-	try{            	
+	try{
             
+            //Get the value to search
+            final String search = jTBusc.getText().trim();
+            
+            //If nothing to search so return
+            if(search.isEmpty()){
+                return;
+            }
+            
+            //Search all the ocurrences
+            bancoTable.getByLikeEncabezados(search);
 	}
 	catch (Exception ex) {
             LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
@@ -128,8 +272,11 @@ public class CatalBancosViewController extends CatalBancosJFrame {
     
     private void jBMosTActionPerformed(java.awt.event.ActionEvent evt) {                                             
 
-	try{            	
+	try{
             
+            bancoTable.loadAllItemsInTable();
+            
+            jTCod.setEditable(true);
 	}
 	catch (Exception ex) {
             LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
@@ -143,38 +290,56 @@ public class CatalBancosViewController extends CatalBancosJFrame {
     
     private void jBActuaActionPerformed(java.awt.event.ActionEvent evt) {                                             
 
-	try{            	
+	try {
             
-	}
-	catch (Exception ex) {
-            LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
-            try {
-                DialogsFactory.getSingleton().getExceptionDialog(baseJFrame, ex).show();
-            } catch (Exception ex1) {
-                Logger.getLogger(CatalBancosViewController.class.getName()).log(Level.SEVERE, null, ex1);
+            if(!bancoTable.isRowSelected()){
+                DialogsFactory.getSingleton().showErrorOKNoSelectionCallbackDialog(baseJFrame, (JFrame jFrame) -> {
+                    bancoTable.grabFocus();
+                });
+                return;
             }
-	}
-    }
-    
-    private void jBImportarActionPerformed(java.awt.event.ActionEvent evt) {                                             
-
-	try{            	
             
-	}
-	catch (Exception ex) {
-            LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
-            try {
-                DialogsFactory.getSingleton().getExceptionDialog(baseJFrame, ex).show();
-            } catch (Exception ex1) {
-                Logger.getLogger(CatalBancosViewController.class.getName()).log(Level.SEVERE, null, ex1);
+            final Banco Banco = (Banco)bancoTable.getRowSelected();
+            
+            final String description = jTDescrip.getText().trim();
+            if(description.isEmpty()){
+                DialogsFactory.getSingleton().showOKEmptyFieldCallbackDialog(baseJFrame, (JFrame jFrame) -> {
+                    jTDescrip.grabFocus();
+                });
+                return;
             }
-	}
-    }
-    
-    private void jBNew1ActionPerformed(java.awt.event.ActionEvent evt) {                                             
-
-	try{            	
             
+            final Banks Bank = (Banks)banksCombobox.getSelectedObject();
+            if(Bank.getName().isEmpty()){
+                DialogsFactory.getSingleton().showOKEmptyFieldCallbackDialog(baseJFrame, (JFrame jFrame) -> {
+                    banksCombobox.grabFocus();
+                });
+                return;
+            }
+            
+            DialogsFactory.getSingleton().showQuestionContinueDialog(baseJFrame, (JFrame jFrame) -> {
+                
+                try {
+                    
+                    Banco.setDescrip(description);
+                    Banco.setBanco(Bank.getCode());                    
+                    
+                    RepositoryFactory.getInstance().getBancosRepository().update(Banco);
+                    
+                    bancoTable.loadAllItemsInTable();
+                    
+                    DialogsFactory.getSingleton().showOKOperationCompletedCallbackDialog(jFrame, (JFrame jFrame1) -> {
+                    });
+                    
+                } catch (Exception ex) {
+                    LoggerUtility.getSingleton().logError(this.getClass(), ex);
+                    try {
+                        DialogsFactory.getSingleton().getExceptionDialog(baseJFrame, ex).show();
+                    } catch (Exception ex1) {
+                        Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex1);
+                    }
+                }
+            });
 	}
 	catch (Exception ex) {
             LoggerUtility.getSingleton().logError(CatalBancosViewController.class, ex);
